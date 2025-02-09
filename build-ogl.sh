53cea69e556a3050a1545343065ed6ec52046ba9
Alexander Pavlov <t.x00100x.t@yandex.ru>
2025-02-09 13:46:59 +0300

    Update appimage window rendering from sdl to opengl.

--- build-ogl.sh
+++ build-ogl.sh
@@ -7,9 +7,12 @@ chmod +x linuxdeploy-x86_64.AppImage
 # get source code
 git clone https://github.com/ioquake/ioq3.git
 
+# add appimage sources
+cp -ax Sources/appimage.* AppDir/usr/bin ioq3/code/qcommon
+
 # apply appimage patch and compile
 cd ioq3
-patch -p1 < ../patches/0001-ioquake3-AppImage.patch
+patch -p1 < ../patches/0002-ioquake3-AppImage.patch
 make -j4
 
 # remove temp stuff
@@ -20,6 +23,7 @@ rm -fr ioq3/build/release-linux-x86_64/missionpack/{cgame,game,qcommon,ui,vm}
 
 # copy files
 cp -ax ioq3/build/release-linux-x86_64/* AppDir/usr/bin
+cp -ax Img/ioquake3.jpg AppDir/usr/bin
 
 # create appimage
 SIGN=1 ./linuxdeploy-x86_64.AppImage --executable AppDir/usr/bin/ioquake3.x86_64 --desktop-file AppDir/usr/share/applications/org.ioquake3.ioquake3.desktop --icon-file AppDir/usr/share/icons/hicolor/128x128/apps/quake3.png --appdir AppDir --output appimage
